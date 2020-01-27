6a928fc5341c75566f30d716d4c3060ff73d5eed
Sascha Dewald <sascha.dewald@gmail.com>
2020-01-27 09:55:45 +0100

    allow gnome under classic

--- snapcraft/internal/project_loader/_extensions/gnome_3_28.py
+++ snapcraft/internal/project_loader/_extensions/gnome_3_28.py
@@ -55,7 +55,7 @@ class ExtensionImpl(Extension):
 
     @staticmethod
     def get_supported_confinement() -> Tuple[str, ...]:
-        return ("strict", "devmode")
+        return ("strict", "devmode", 'classic')
 
     def __init__(self, *, extension_name: str, yaml_data: Dict[str, Any]) -> None:
         super().__init__(extension_name=extension_name, yaml_data=yaml_data)
